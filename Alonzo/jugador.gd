852ae380e20f2f57125f67bcd63644ce0698a6ac
Skull33 <145301098+Skull33@users.noreply.github.com>
2026-01-28 17:18:43 -0500

    Añdiendo el proyecto a mi rama

---
+++ Alonzo/jugador.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const SPEED := 300.0
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	var mouse_pos = get_global_mouse_position()
+	var mouse_dir = (mouse_pos - global_position).normalized()
+	rotation = mouse_dir.angle()
+
+	# Entrada WASD (Recien antes eran las direccionales)
+	if Input.is_action_pressed("Izquierda"):
+		direction.x -= 1
+	if Input.is_action_pressed("Derecha"):
+		direction.x += 1
+	if Input.is_action_pressed("Adelante"):
+		direction.y -= 1
+	if Input.is_action_pressed("Atras"):
+		direction.y += 1
+
+	# Normalizar para que no vaya más rápido en diagonal
+	direction = direction.normalized()
+
+	# Aplicar velocidad
+	velocity = direction * SPEED
+
+	move_and_slide()
---
+++ puerta_madera.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var max_angle = 90
+@export var open_vel = 6
+
+var is_open = false
+var target_angle = 0
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
