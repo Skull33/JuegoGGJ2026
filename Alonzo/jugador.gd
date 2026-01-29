9d507746591f646c6be9258c536921072c3afb60
Skull33 <145301098+Skull33@users.noreply.github.com>
2026-01-28 22:53:18 -0500

    añadi puerta, trabajando en interacciones con objetos

--- Alonzo/jugador.gd
+++ Alonzo/jugador.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED := 300.0
+const SPEED := 100.0
 
 func _process(delta):
 	var direction = Vector2.ZERO
---
+++ caja.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var spaws_llave = [
+	$Marker2D,
+	$Marker2D2
+]
+@onready var anims = $AnimationPlayer
+
+var abierto = false
+
+func _process(delta):
+	var i = spaws_llave.pick_random()
